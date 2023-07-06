0e4aef1ae3f635fffff431c9862ec3c90d5dc054
Rubin Raithel <33808743+Coronon@users.noreply.github.com>
2023-07-06 21:29:47 +0200

    feat: implement basic SMTP server

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/coronon/pingpong-email
 
 go 1.20
+
+require github.com/chrj/smtpd v0.3.1
--- main.go
+++ main.go
@@ -1,5 +1,40 @@
 package main
 
+import (
+	"flag"
+	"fmt"
+
+	"github.com/chrj/smtpd"
+)
+
+var (
+	welcomeMsg = flag.String("welcome", "PingPong email tester", "Welcome message for SMTP session")
+	inAddr     = flag.String("inaddr", "localhost:25", "Address to listen for incoming SMTP on")
+	inbox      = flag.String("inbox", "*", "Inbox address to receive email for")
+)
+
+// Check valid recipient (if limited)
+func recipientChecker(peer smtpd.Peer, addr string) error {
+	if *inbox != "*" && addr != *inbox {
+		return fmt.Errorf("Please send your test emails to: %v", *inbox)
+	}
+
+	return nil
+}
+
+func handler(peer smtpd.Peer, env smtpd.Envelope) error {
+	return nil
+}
+
 func main() {
-	
+	flag.Parse()
+
+	server := &smtpd.Server{
+		WelcomeMessage:   *welcomeMsg,
+		RecipientChecker: recipientChecker,
+		Handler:          handler,
+	}
+
+	fmt.Println("Starting server...")
+	server.ListenAndServe(*inAddr)
 }
